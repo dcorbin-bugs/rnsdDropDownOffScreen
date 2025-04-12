6bb3cc9d5f01b3839d73d67c7feb7a5102411a8b
dcorbin <dave@dcorbin.com>
2025-04-12 09:11:21 -0400

    show two dropdowns

--- App.tsx
+++ App.tsx
@@ -1,9 +1,34 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
 
 const data = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot'];
+
+function Pick6(props: {y: number; label: string}) {
+  return (
+    <SelectDropdown
+      data={data}
+      renderButton={() => {
+        return (
+          <View style={{left: 150, top: props.y, backgroundColor: '#fcc'}}>
+            <Text style={{fontSize: 40}}>{props.label}</Text>
+          </View>
+        );
+      }}
+      renderItem={(item, index, isSelected) => {
+        return (
+          <View style={{backgroundColor: isSelected ? '#ccf' : '#fff'}}>
+            <Text style={{fontSize: 40}}>{item}</Text>
+          </View>
+        );
+      }}
+      onSelect={(item, index) => {
+        console.log(`Selected; ${item} @ ${index}`);
+      }}
+    />
+  );
+}
 
 function App(): React.JSX.Element {
   return (
@@ -13,26 +38,8 @@ function App(): React.JSX.Element {
         height: '100%',
         backgroundColor: '#ccc',
       }}>
-      <SelectDropdown
-        data={data}
-        renderButton={() => {
-          return (
-            <View style={{left: 150, top: 420, backgroundColor: '#fcc'}}>
-              <Text style={{fontSize: 40}}>Dropdown</Text>
-            </View>
-          );
-        }}
-        renderItem={(item, index, isSelected) => {
-          return (
-            <View style={{backgroundColor: isSelected ? '#ccf' : '#fff'}}>
-              <Text style={{fontSize: 40}}>{item}</Text>
-            </View>
-          );
-        }}
-        onSelect={(item, index) => {
-          console.log(`Selected; ${item} @ ${index}`);
-        }}
-      />
+      <Pick6 y={200} label={'works'} />
+      <Pick6 y={420} label={'broken'} />
     </View>
   );
 }
